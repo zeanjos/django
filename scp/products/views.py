825eb50f382cee09d454572c8cbaa4cb1d352585
zeanjos <pegananjos@gmail.com>
2025-04-28 23:27:45 -0300

    novos aprendizados.

--- scp/products/views.py
+++ scp/products/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
 # Create your views here.
+
+def product_view(request):
+    return render(
+        request = request,
+        template_name = 'products.html',
+    )
+
+
